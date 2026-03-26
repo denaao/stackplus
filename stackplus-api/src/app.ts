2775dc565c5755771978aecb272294ac4b4c5a22
estefanidex <denis.poker10@gmail.com>
2026-03-26 16:09:24 -0300

    allow all localhost ports in cors

--- stackplus-api/src/app.ts
+++ stackplus-api/src/app.ts
@@ -12,6 +12,9 @@ app.use(helmet())
 const allowedOrigins = [
   process.env.FRONTEND_URL,
   'http://localhost:3000',
+  'http://localhost:3001',
+  'http://localhost:3002',
+  'http://localhost:3003',
 ].filter(Boolean) as string[]
 
 app.use(cors({
